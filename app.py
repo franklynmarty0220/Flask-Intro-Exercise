793341edbb2cc529a9e302010f14df9047e7f40c
franklynmarty0220 <64612023+franklynmarty0220@users.noreply.github.com>
2022-04-30 21:45:34 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,42 +1,15 @@
-# Put your app in here.
-from flask import Flask, request
-from operations import add, sub, mult, div
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/add")
-def add():
-    """Add a and b parameters."""
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))    
-    result = add(a, b)
+@app.route('/welcome')
+def welcome():
+    return 'Welcome'
 
-    return str(result)
+@app.route('/welcome/back')
+def welcome_back():
+    return "Welcome Back"
 
-@app.route("/sub")
-def sub():
-    """Subract a and b parameters."""
-    a= int(request.args.get("a"))
-    b= int(request.args.get("b"))
-    result = sub(a,b)
-
-    return str(result)
-
-@app.route("/mult")
-def mult():
-    """Multiply a and b parameters."""
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))   
-    result = mult(a,b)
-
-    return str(result)
-
-@app.route("/div")
-def div():
-    """Divide a and b parameters."""
-
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))  
-    result = div(a,b)
-
-    return str(result)
+@app.route('/welcome/home')
+def welcome_home():
+    return "Welcome Home"
